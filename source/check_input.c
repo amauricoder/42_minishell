6fd42cc0bea5db74465ca66bf7088aaaefef6c1b
Isabella Miranda Camargo De Lima <ismirand@c2r4s2.42porto.com>
2024-09-05 14:47:29 +0100

    fix syntax error in closed quotes

--- source/check_input.c
+++ source/check_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aconceic <aconceic@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: ismirand <ismirand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 09:07:33 by aconceic          #+#    #+#             */
-/*   Updated: 2024/08/23 15:16:26 by aconceic         ###   ########.fr       */
+/*   Updated: 2024/09/05 14:46:51 by ismirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,12 @@ int	is_argument_valid(int argc, char **env)
 */
 int	is_input_invalid(t_mini *mini, char *input)
 {
-	if (!is_quotes_closed(input) || is_pipe_last_or_first(input)
-		|| is_redir_invalid(input))
-		return (err_msg(mini, SYNTAX_ERR, 2, 0));
+	if (!is_quotes_closed(input) || is_quotes_closed(input) < 0)
+	{
+		if (!is_quotes_closed(input) || is_redir_invalid(input)
+			|| is_pipe_last_or_first(input))
+			return (err_msg(mini, SYNTAX_ERR, 2, 0));
+	}
 	return (EXIT_SUCCESS);
 }
 
@@ -46,6 +49,7 @@ int	is_input_invalid(t_mini *mini, char *input)
  * @brief Check if the quotes are closed.
  * @return TRUE if s_quotes and d_quotes are closed
  * 			FALSE if one of them is opened
+ * 			-1 if is not in quotes
  * first if / Considera apenas se não estamos dentro de aspas dupla
  * second if / Considera apenas se não estamos dentro de aspas simples
 */
@@ -66,5 +70,7 @@ int	is_quotes_closed(char *input)
 			d_quotes++;
 		i++;
 	}
+	if (!s_quotes && !d_quotes)
+		return (-1);
 	return (s_quotes % 2 == 0 && d_quotes % 2 == 0);
 }
